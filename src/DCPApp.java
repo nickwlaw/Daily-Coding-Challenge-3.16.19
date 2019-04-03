0ed3f821ef6e0e44a522a277c920222f0320b16c
Nicholas Law <nick@nickwlaw.com>
2019-04-03 15:07:16 -0400

    Updated test2 method to attempt to fix incorrect calculations. test2 still not functioning properly.

--- src/DCPApp.java
+++ src/DCPApp.java
@@ -6,8 +6,8 @@ public class DCPApp {
 		
 		int[] nums = {1, 2, 3};
 		System.out.println(Arrays.toString(nums));
-		test2(nums);
-		System.out.println(Arrays.toString(nums));
+		int[] numsAltered = test2(nums);
+		System.out.println(Arrays.toString(numsAltered));
 
 	}
 	
@@ -26,14 +26,25 @@ public class DCPApp {
 	
 	// set new values by multiplying each index by each other index -- NOT WORKING PROPERLY
 	static int[] test2(int[] nums) {
-		
+	
+		int[] numsAltered = nums.clone();
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < nums.length; j++) {
 				if (i != j)
-					nums[i] *= nums[j];
+					numsAltered[i] *= nums[i];
 			}
 		}
-		return nums;
+		return numsAltered;
 	}
 
 }
+
+/* Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers 
+ * in the original array except the one at i.
+ * 
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1],
+ * the expected output would be [2, 3, 6].
+ * 
+ * Follow-up: what if you can't use division?
+ * 
+ */
